1468e3017b82213ae8a24919f20363f3cd901cc0
Gilbert Fine <gilbert.fine@gmail.com>
2015-04-23 17:47:56 +0800

    RpcInPacket hasve 3 ctor: request, response, notification

--- msgpack-rpc/Network/MessagePackRpc/Types.hs
+++ msgpack-rpc/Network/MessagePackRpc/Types.hs
@@ -5,6 +5,7 @@ module Network.MessagePackRpc.Types (
   RpcInPacket(..),
   ) where
 
+import Data.Bits
 import Control.Applicative
 import Data.MessagePack
 
@@ -19,24 +20,37 @@ type RpcMessageId = Int
 type Response = (RpcMessageType, RpcMessageId, Object, Object)
 
 type Request  = (RpcMessageType, RpcMessageId, String, [Object])
+
 type Notification  = (RpcMessageType, String, [Object])
 
 -- | Incoming data from peer
 data RpcInPacket =  RpcInRequest Request
                     | RpcInNotification Notification
+                    | RpcInResponse Response
                     deriving (Show, Eq)
 
 instance Packable RpcInPacket where
     from (RpcInRequest x)       = from x
     from (RpcInNotification x)  = from x
+    from (RpcInResponse x)      = from x
 
 instance Unpackable RpcInPacket where
-    get = get_request <|> get_notification
+    get = get_re <|> get_notification
         where
-            get_request = RpcInRequest <$> get
+            get_re = do
+                (rtype, msgid, obj3, obj4) <- get
+                if rtype .&. 1 == 0
+                    then do
+                        name <- either fail return $ tryFromObject obj3
+                        args <- either fail return $ tryFromObject obj4
+                        return $ RpcInRequest (rtype, msgid, name, args)
+                    else
+                        return $ RpcInResponse (rtype, msgid, obj3, obj4)
+
             get_notification = RpcInNotification <$> get
 
 instance OBJECT RpcInPacket where
     toObject (RpcInRequest x)       = toObject x
     toObject (RpcInNotification x)  = toObject x
+    toObject (RpcInResponse x)      = toObject x
 
